f0880c0f485b5830cb8327cd443e94841149a3a3
Juho Vepsalainen <bebraw@gmail.com>
2013-06-13 11:36:52 +0300

    Add a stub for favicon module
    This module should be able to convert an ip to a favicon. We will use
    the link later on at the API.

---
+++ favicon.js
@@ -0,0 +1,9 @@
+var providers = {
+    'Amazon': 'http://www.amazon.com/favicon.ico'
+    // TODO: fill these
+};
+
+exports.ipToFavicon = function(ip, cb) {
+    // TODO: this should convert given ip into a favicon link (returned at cb)
+    // TODO: consider using some caching scheme here for favicons
+}
