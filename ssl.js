83514adf1de5140114fe4067c389c9bd986edab0
Juho Vepsalainen <bebraw@gmail.com>
2013-07-23 21:40:23 +0300

    Parse ssl related information
    Solves most of #3. We still need to discuss details and finalize the
    output format.

--- ssl.js
+++ ssl.js
@@ -1,17 +1,94 @@
+var fs = require('fs');
+var extend = require('util')._extend;
+
+var async = require('async');
 var openssl = require('openssl-wrapper');
+var temp = require('temp');
+var x509 = require('x509');
+
 
 exports.connect = connect;
 
 function connect(host, cb) {
     console.log(host + ':443')
     openssl.exec('s_client', {connect: host + ':443'}, function(err, buffer) {
-        var d = buffer.toString();
+        var d = buffer.toString().split('\n---\n').filter(id);
 
-        d = [d];
-
-        // TODO: parse data now
-        if(d) return cb(null, d);
-
-        cb(err);
+        if(d) {
+            async.map([
+                certificateChain,
+                certificate,
+                info
+            ], function(fn, cb) {
+                fn(d, cb);
+            }, function(err, d) {
+                cb(err, toObject(d));
+            });
+        }
     })
 }
+
+function toObject(arr) {
+    var ret = arr.shift();
+
+    arr.forEach(function(v) {
+        ret = extend(ret, v);
+    });
+
+    return ret;
+}
+
+function certificateChain(d, cb) {
+    var ret = [];
+
+    d[1].split('\n').slice(1).forEach(function(v) {
+        var parts = v.split('s:');
+
+        if(parts.length >= 2) {
+            var p = parts.slice(1).join('');
+
+            if(ret.indexOf(p) == -1) ret.push(p);
+        }
+        else {
+            ret.push(v.split('i:').slice(1).join(''));
+        }
+    });
+
+    cb(null, {
+        chain: ret
+    });
+}
+
+function certificate(d, cb) {
+    var parts = d[2].split('\n');
+    var certData = parts.slice(1, parts.length - 2).join('\n');
+
+    temp.open('cert', function(err, cert) {
+        if(err) return cb(err);
+
+        fs.writeFile(cert.path, certData, function(err) {
+            if(err) return cb(err);
+
+            fs.close(cert.fd, function(err) {
+                cb(null, {
+                    certificate: x509.parseCert(cert.path)
+                });
+            });
+        })
+    });
+}
+
+function info(d, cb) {
+    var ret = d[d.length - 1].split('SSL-Session:')[0].split('\n');
+
+    cb(null, {
+        info: {
+            'type': ret[0].split(',')[1].trim(),
+            key: ret[1].split(' is ')[1].trim(),
+            compression: ret[3].split(':')[1].trim(),
+            expansion: ret[4].split(':')[1].trim()
+        }
+    });
+}
+
+function id(a) {return a;}
