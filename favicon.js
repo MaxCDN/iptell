02869be25517dc758c3d07e515e0b254d43e211b
jdorfman <justindorfman@gmail.com>
2013-07-23 11:47:47 -0700

    bluehost, digitalocean, softlayer

--- favicon.js
+++ favicon.js
@@ -1,6 +1,8 @@
 var providers = {
-    'Amazon': 'http://www.amazon.com/favicon.ico',
-    'Unified Layer': 'http://www.bluehost.com/favicon.ico',
+    'Amazon Web Services': 'http://aws.amazon.com/favicon.ico',
+    'Bluehost': 'http://www.bluehost.com/favicon.ico',
+    'DigitalOcean': 'http://www.digitalocean.com/favicon.ico',
+    'Softlayer': 'http://www.softlayer.com/favicon.ico',
     'SingleHop': 'http://www.singlehop.com/favicon.ico'
     // TODO: add more
 };
